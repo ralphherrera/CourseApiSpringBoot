405103d54dda26ce6404a503dc739cfd400e72d4
Herrera, Ralph Paulo <ralph.herrera@pointwest.com.ph>
2018-04-10 14:22:21 +0800

    [ralph.herrera 4/10] Initial commit

---
+++ src/main/java/com/rph/springboot/CourseApi/CourseApiApplication.java
@@ -0,0 +1,12 @@
+package com.rph.springboot.CourseApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApplication.class, args);
+	}
+}
